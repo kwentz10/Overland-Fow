1b69887419773bbfa23a1fefa2643143cf57febb
kwentz10 <katherinewentz10@gmail.com>
2016-04-30 14:16:50 -0600

    Update q_func.m

--- q_func.m
+++ q_func.m
@@ -5,7 +5,7 @@ h_avg = havg_func(h);
 slope = slope_func(z,dx);
 u=(1/n).*(h_avg.^(2/3)).*((abs(slope)).^(1/2));
 q=u.*h_avg;
-q=[q(1),q,q(end)];
+q=[0,q,q(end)]; %no water flux into the top, boundary condition
 
 end
 
